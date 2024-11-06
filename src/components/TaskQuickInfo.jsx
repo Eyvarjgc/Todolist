2ed8c6f689b8534e910dcb64a886db41fd7fa4e2
Eyvar <eyvar31@gmail.com>
2024-11-05 19:21:04 -0500

    Task quick view redesigned

--- src/components/TaskQuickInfo.jsx
+++ src/components/TaskQuickInfo.jsx
@@ -2,8 +2,8 @@ import { useRouteError } from "react-router-dom"
 import { useState } from "react"
 
 export function TaskQuickInfo({task,desc }){
-  const taskName = task.length > 10 ? task.slice(0, 10) : task;
-  const description = desc.length >= 15 ? desc.slice(0, 15) : desc;
+  const taskName = task.length > 20 ? task.slice(0, 20) : task;
+  const description = desc.length >= 30 ? desc.slice(0, 30) : desc;
 
 
   
@@ -11,33 +11,39 @@ export function TaskQuickInfo({task,desc }){
   return(
     //     
     // flex gap-1 justify-center 
-    <div className="taskQuickView p-2 w-[30%] border bg-opacity-50
-     flex justify-center gap-1 bg-gradient-to-r from-black
-      bg-orange-900 rounded-2xl" >
+    <div className="taskQuickView p-2 w-full lg:h-[45px] bg-opacity-90 
+     flex  gap-1 bg-gradient-to-l from-black bg-orange-900  rounded-2xl" >
 
-      <span className=" w-fit ">
-      <input id="default-checkbox" type="checkbox" value="" class="appearance-none w-4 h-4 border  border-white rounded-2xl checked:bg-white mt-2 " />
+{/* Check button */}
+      <span className="  ">
+      <input id="default-checkbox" type="checkbox" value="" class="appearance-none w-4 h-4
+       border  border-white rounded-2xl checked:bg-white  " />
 
       </span>
 
-      <span className="flex flex-wrap flex-col  w-3/4 ">
 
-       <h2 className="font-semibold text-lg  text-wrap w-fit break-all">{taskName}</h2>
-      <p className="font-thin text-opacity-55 text-gray-50 text-xs">{description}</p>
+{/* Task name and descirption */}
+      <span className="flex flex-wrap flex-col  w-full  justify-center ">
 
-      <span className="flex text-gray-50 text-opacity-55 text-xs">
+      <span className="w-fit ">
+        <h2 className="font-semibold text-xl  text-wrap w-fit break-all capitalize">{taskName}</h2>
+        <p className="font-thin text-opacity-55 text-gray-50 text-xs">{description}</p>
+      </span>
+
+      <span className="flex text-gray-50 text-opacity-55 text-xs items-end justify-end  mr-4">
         <p>oct 13</p>
         <span>|</span>
         <p>Exciting</p>
       </span>
       </span>
 
-      <span className="flex flex-col  w-1/6 items-end  justify-end gap-1">
-        <button className="bg-black bg-opacity-70 border border-orange-700 w-8 h-8 rounded-md ">
+{/* Buttons delete and edit */}
+      <span className="flex  items-center gap-4">
+        <button className="bg-black bg-opacity-70 border p-1 border-orange-700 w-8 h-8 rounded-md ">
           <img src="/img/icons/deleteIcon.svg" alt="" className="" />
         </button>
 
-        <button className="bg-black bg-opacity-70 border border-orange-700 w-8 h-8  rounded-md ">
+        <button className="bg-black bg-opacity-70 border p-1 border-orange-700 w-8 h-8  rounded-md ">
           <img src="/img/icons/editIcon.svg" alt="" className=""/>
         </button>
       </span>
--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,29 +1,26 @@
 import { useContext, useState, useEffect } from "react"
-
+import { useAppContext } from "../Hooks/useAppContext"
+import { TaskQuickInfo } from "../components/TaskQuickInfo"
 
 export function Today(){
-  const {taskObject} = useContext(AppContext)
-  const listItem = taskObject.map((item,index) => 
-    <div key={index}>
-      <p>{item.name}</p>
-      <p>{item.description}</p>
-    </div>
+  const {taskObject} = useAppContext()
+
+
+  const listItem =taskObject.map((item,index) => 
+    <TaskQuickInfo desc={item.description} key={index} task={item.name} />
   )
+  
 
-
-   return(
-    <div className='orbitron  absolute  w-full left-0  lg:relative '>
-    <h1 className='text-xl text-center lg:text-start'>Dashboard - Today task</h1>
-    <div className='orbitron text-sm mt-4'>
-
+  return(
+    <div className='orbitron   absolute  w-full left-0   lg:static    z-10 
+     lg:p-0 '>
+    <h1 className='text-xl text-center lg:text-start '>Dashboard</h1>
+    <div className='orbitron text-sm mt-4 z-40 flex gap-8 flex-wrap '>
     {listItem}
-
-    </div>
-
-
-
+    
     
 
+    </div>
     </div>
   )
 }
