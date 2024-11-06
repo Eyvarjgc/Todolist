7245e370ba115458b7526829cd2fb33bd7d31d0a
Eyvar <eyvar31@gmail.com>
2024-11-05 19:47:50 -0500

    task quick view responsive

--- src/components/TaskQuickInfo.jsx
+++ src/components/TaskQuickInfo.jsx
@@ -11,26 +11,26 @@ export function TaskQuickInfo({task,desc }){
   return(
     //     
     // flex gap-1 justify-center 
-    <div className="taskQuickView p-2 w-full lg:h-[45px] bg-opacity-90 
-     flex  gap-1 bg-gradient-to-l from-black bg-orange-900  rounded-2xl" >
+    <div className="taskQuickView p-2 w-full h-12 md:h-[45px] bg-opacity-90 
+     flex  gap-1 bg-gradient-to-l from-black bg-orange-900  rounded-xl " >
 
 {/* Check button */}
       <span className="  ">
       <input id="default-checkbox" type="checkbox" value="" class="appearance-none w-4 h-4
-       border  border-white rounded-2xl checked:bg-white  " />
+       border  border-white rounded-2xl checked:bg-white mt-1 " />
 
       </span>
 
 
 {/* Task name and descirption */}
-      <span className="flex flex-wrap flex-col  w-full  justify-center ">
+      <span className="flex  w-full flex-wrap items-center justify-between">
 
-      <span className="w-fit ">
-        <h2 className="font-semibold text-xl  text-wrap w-fit break-all capitalize">{taskName}</h2>
-        <p className="font-thin text-opacity-55 text-gray-50 text-xs">{description}</p>
+      <span className="w-fit gap-0">
+        <p className="font-semibold text-wrap w-fit break-all capitalize text-[10px]  sm:text-sm transition-all ">{taskName}</p>
+        <p className="font-thin text-opacity-55 text-gray-50 text-[8px] sm:text-xs  transition-all hidden md:block">{description}</p>
       </span>
 
-      <span className="flex text-gray-50 text-opacity-55 text-xs items-end justify-end  mr-4">
+      <span className=" text-gray-50 text-opacity-55 text-[10px] md:text-xs mr-4  transition-all flex">
         <p>oct 13</p>
         <span>|</span>
         <p>Exciting</p>
@@ -38,12 +38,12 @@ export function TaskQuickInfo({task,desc }){
       </span>
 
 {/* Buttons delete and edit */}
-      <span className="flex  items-center gap-4">
-        <button className="bg-black bg-opacity-70 border p-1 border-orange-700 w-8 h-8 rounded-md ">
+      <span className="flex  items-center gap-2 md:gap-4 transition-all">
+        <button className="bg-black bg-opacity-70 border p-1 border-orange-700 w-6  md:w-7 md:h-7 rounded-md ">
           <img src="/img/icons/deleteIcon.svg" alt="" className="" />
         </button>
 
-        <button className="bg-black bg-opacity-70 border p-1 border-orange-700 w-8 h-8  rounded-md ">
+        <button className="bg-black bg-opacity-70 border p-1 border-orange-700 w-6 md:w-7 md:h-7  rounded-md ">
           <img src="/img/icons/editIcon.svg" alt="" className=""/>
         </button>
       </span>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -23,7 +23,7 @@ export function Homepage(){
     <div className='orbitron   absolute  w-full left-0   lg:static    z-10 
      lg:p-0 '>
     <h1 className='text-xl text-center lg:text-start '>Dashboard</h1>
-    <div className='orbitron text-sm mt-4 z-40 flex gap-8 flex-wrap '>
+    <div className='orbitron text-sm mt-4 z-40 flex gap-4 flex-wrap px-12 lg:px-0 '>
     {listItem}
     
     
