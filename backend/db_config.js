c5e36410ecc650b40f5db8c2faa959648bdcb25c
Eyvar <eyvar31@gmail.com>
2025-05-26 22:05:21 -0500

    fix: env variables for db

--- backend/db_config.js
+++ backend/db_config.js
@@ -1,11 +1,12 @@
-export const HOST = process.env.HOST || '127.0.0.1'
-export const USER = process.env.USER || 'root'
-export const  DB_PORT = process.env.PORT || '3306'
-export const  DATABASE = process.env.DATABASE || 'todo'
-export const  ORIGIN = process.env.ORIGIN || 'http://localhost:5173'
+export const HOST = process.env.HOST 
+export const USER = process.env.USER  
+export const  DB_PORT = process.env.DB_PORT  
+export const  DATABASE = process.env.DATABASE  
+
+export const  ORIGIN = process.env.ORIGIN
 
 
-export const  PASSWORD = process.env.PASSWORD || '';
-export const SECRET_KEY = process.env.SECRET_KEY || ''
-export const REFRESH_KEY = process.env.REFRESH_KEY || ''
-export const CLIENTID = process.env.CLIENTID || ''
+export const  PASSWORD = process.env.PASSWORD  
+export const SECRET_KEY = process.env.SECRET_KEY  
+export const REFRESH_KEY = process.env.REFRESH_KEY  
+export const CLIENTID = process.env.CLIENTID  
